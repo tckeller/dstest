469e8d6f015d9daf8ba11b9e21ce35389c2327fd
Tim Keller <timkeller@protonmail.ch>
2024-01-20 23:34:44 +0100

    Add initial readme, setup.py and requirements

---
+++ Readme.md
@@ -0,0 +1,5 @@
+DSTest
+======
+
+I have an idea for a data science productivity framework based on unit testing. 
+This is a Work in progress. Let's see where it goes. 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DSTest',
+    version='0.0.1',
+    url='https://github.com/mypackage.git',
+    author='Tim Keller',
+    author_email='',
+    description='A framework for data science experimentation by unit testing',
+    packages=find_packages(),    
+    install_requires=[],
+)
